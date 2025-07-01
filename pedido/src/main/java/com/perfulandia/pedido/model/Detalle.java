c561077af25bc3f80976acee0781b387d97c1dd1
maaurissio <maur.gajardo@duocuc.cl>
2025-07-01 01:30:28 -0400

    aa

--- pedido/src/main/java/com/perfulandia/pedido/model/Detalle.java
+++ pedido/src/main/java/com/perfulandia/pedido/model/Detalle.java
@@ -1,5 +1,6 @@
 package com.perfulandia.pedido.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,16 +23,23 @@ public class Detalle {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID dela detalle", example = "1")
     private Long idDetalle;
 
+    @Schema(description = "ID del producto", example = "1")
     private int idProducto; //Simulando la clase producto
 
+    @Schema(description = "Cantidad de productos", example = "20")
     private int cantidad; //Cantidad de productos
+
+    @Schema(description = "Precio unitario del producto", example = "1000.0")
     private double precioUnitario; //Precio unitario del producto
+
+    @Schema(description = "SubTotal del detalle (cantidad * precioUnitario)", example = "20000.0")
     private double subTotal; //SubTotal del detalle (cantidad * precioUnitario)
 
-    // Campo para recibir solo el ID del pedido en el JSON
     @Transient
+    @Schema(description = "ID del pedido al que pertenece el detalle", example = "1")
     private Long idPedido;
 
     @ManyToOne()
--- pedido/src/main/java/com/perfulandia/pedido/model/Pedido.java
+++ pedido/src/main/java/com/perfulandia/pedido/model/Pedido.java
@@ -4,6 +4,7 @@ import java.time.LocalDateTime;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -26,20 +27,26 @@ public class Pedido {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(description = "ID del pedido", example = "1")
     private Long idPedido;
 
+    @Schema(description = "ID del cliente", example = "12")
     @Column(length = 10, nullable = true)
     private int idCliente;
 
+    @Schema(description = "Estado del pedido", example = "CREADO")
     @Enumerated(EnumType.STRING)
     private EstadoPedido estadoPedido;
 
+    @Schema(description = "Dirección de envío del pedido", example = "Coronel")
     @Column(length = 250, nullable = true)
     private String direccionEnvio;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "America/Santiago")
+    @Schema(description = "Fecha de creación del pedido", example = "2023-10-01T12:00:00")
     private LocalDateTime fechaCreacion;
 
+    @Schema(description = "Total del pedido (suma de los subtotales de los detalles)", example = "15000.0")
     private double total; //Total del pedido (suma de los subtotales de los detalles)
 
     //Metodo para actualizar el estado del pedido
